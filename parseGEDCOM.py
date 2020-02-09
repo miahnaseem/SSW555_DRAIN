6c6b25b64e6e2bc67cda8338092413cb1a7d88af
Naseem Miah <nmiah@stevens.edu>
2020-02-09 11:19:42 -0500

    parseGEDCOM now adds individuals and families to respective dict. Dates need to be nested

--- parseGEDCOM.py
+++ parseGEDCOM.py
@@ -5,6 +5,14 @@ tags = {
     "INDI": "0", "NAME": "1", "SEX": "1", "BIRT": "1", "DEAT": "1", "FAMC": "1", "FAMS": "1", "FAM": "0", "MARR": "1",
     "HUSB": "1", "WIFE": "1", "CHIL": "1", "DIV": "1", "DATE": "2", "HEAD": "0", "TRLR": "0", "NOTE": "0"
 }
+# Dictionaries to save information about individuals or families
+indi = {}
+
+fam = {}
+
+current = ""
+which_dict = ""
+
 f = open(sys.argv[1], "r")
 while True:
     # Get one line of the GEDCOM file at a time
@@ -14,48 +22,63 @@ while True:
     if not inp:
         break
 
-    print("--> " + inp)
-
     # Splits the current line by spaces and puts it into an array
     line = inp.split()
 
-    # Build output string little by little
-    out = "<-- " + line[0] + "|"
+    # Initialize Variables
     arg = ""
+    tag = ""
     valid = False
+    date = False
 
     # For every word in the current line, checks if it is in the dictionary of tags.
-    # If it is in the dictionary and it is at the right level, build the output with Y
-    # If it is in the dictionary but isn't at the right levle, build the output with N
+    # If tag is in the dictionary and it is at the right level, tag is valid
+    # If tag is in the dictionary but isn't at the right level, tag is invalid
     for arg in line:
         if arg in tags.keys() and tags[arg] == line[0]:
             valid = True
-            out += arg + "|Y|"
             break
         elif arg in tags.keys():
-            valid = True
-            out += arg + "|N|"
+            valid = False
             break
-    
-    # Check the two special cases where the tag comes after the argument.
-    if (arg == "INDI" or arg == "FAM") and line[2] != arg:
-        out = "<-- " + line[0] + "|" + arg + "|N|"
-    
+
+    # Check the two special cases where the we add indi or fam dictionary entry
+    if line[0] == "0":
+        if arg == "INDI":
+            current = line[1]
+            which_dict = "INDI"
+            indi[current] = {}
+
+        if arg == "FAM":
+            current = line[1]
+            which_dict = "FAM"
+            fam[current] = {}
+
     # Gets rid of the tag from the array
     if arg in tags.keys():
         line.remove(arg)
 
-    # Accounts for the other possible tags that aren't in the dictionary of valid tags
-    if not valid and len(line) > 1 and (line[1].isupper() or "_" in line[1]):
-        out = "<-- " + line[0] + "|" + line[1] + "|N|"
-        del line[1]
-
     # Deletes the level from the array
     del line[0]
 
-    # Adds the arguments to the output string
-    for x in line:
-        out += x + " "
-    print(out.strip())
 
+    if which_dict == "INDI" and arg != "INDI" and valid == True:
+        indi[current][arg] = ' '.join(line)
+
+    if which_dict == "FAM" and arg != "FAM" and valid == True:
+        fam[current][arg] = ' '.join(line)
+
+    # Accounts for the other possible tags that aren't in the dictionary of valid tags
+    # if not valid and len(line) > 1 and (line[1].isupper() or "_" in line[1]):
+
+        # del line[1]
+
+    
+
+    # Adds the arguments to the output string
+    # for x in line:
+    #     out += x + " "
+
+print(indi)
+print(fam)
 f.close()
