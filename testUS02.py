1af3d818e408a61a6d77f0c03b1375e36f273851
Naseem Miah <nmiah@stevens.edu>
2020-02-22 15:01:13 -0500

    Update unit test

--- testUS02.py
+++ testUS02.py
@@ -3,7 +3,7 @@ import parseGEDCOM
 
 class us02Test(unittest.TestCase):
     def testUS02(self):
-        self.assertEqual(parseGEDCOM.checkUS02(), "ERROR: FAMILY: US09: @F5@: Birthday of (@I13@) on 2006-11-06 after husband's (@I12@) death on 1999-04-09\nERROR: FAMILY: US09: @F9@: Birthday of (@I17@) on 1974-11-01 after wife's (@I23@) death on 1973-02-05\nERROR: FAMILY: US09: @F10@: Birthday of (@I21@) on 1760-05-10 after husband's (@I25@) death on 1750-11-29 and wife's (@I24@) death on 1750-07-20\n")
+        self.assertEqual(parseGEDCOM.checkUS02(), "ERROR: INDIVIDUAL: US02: @I17@: Marriage is before Birth date 1974-11-01")
 
 if __name__ == "__main__":
     unittest.main()
