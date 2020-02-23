1d50ad703a4f421d6f62aaac0301c0ceaf85a45d
ijoyce833 <ijoyce833@gmail.com>
2020-02-23 17:07:43 -0500

    fixed errors with unit tests

--- testUS03.py
+++ testUS03.py
@@ -3,7 +3,10 @@ import parseGEDCOM
 
 class us03Test(unittest.TestCase):
     def testUS03(self):
-        self.assertEqual(parseGEDCOM.checkUS02(), "ERROR: INDIVIDUAL: US03: @I4@: Death date 1994-11-10 is before Birth date 2004-10-12")
+        self.assertEqual(parseGEDCOM.checkUS03(),
+        	"ERROR: INDIVIDUAL: US03: @I4@: Death date 1994-11-10 is before Birth date 2004-10-12\n"+
+        	"ERROR: INDIVIDUAL: US03: @I16@: Death date 1970-07-02 is before Birth date 1975-04-05\n"
+        	)
 
 if __name__ == "__main__":
     unittest.main()
