f3ea5abb0cc9efe9208b4a7f587cc5eb83a48c89
ijoyce833 <ijoyce833@gmail.com>
2020-03-08 19:01:31 -0400

    added/fixed relevant tests for US14 and US15

--- testUS01.py
+++ testUS01.py
@@ -4,7 +4,23 @@ import parseGEDCOM
 class us01Test(unittest.TestCase):
     def testUS01(self):
         self.assertEqual(parseGEDCOM.checkUS01(), 
-        	"ERROR: INDIVIDUAL: US01: @I2@: Birth date 2070-07-04 is after current date 2020-03-07\n")
+        	"ERROR: INDIVIDUAL: US01: @I2@: Birth date 2070-07-04 is after current date 2020-03-08\n"+
+        	"ERROR: INDIVIDUAL: US01: @I30@: Birth date 2027-07-07 is after current date 2020-03-08\n"+
+			"ERROR: INDIVIDUAL: US01: @I31@: Birth date 2027-07-07 is after current date 2020-03-08\n"+
+			"ERROR: INDIVIDUAL: US01: @I32@: Birth date 2027-07-07 is after current date 2020-03-08\n"+
+			"ERROR: INDIVIDUAL: US01: @I33@: Birth date 2027-07-07 is after current date 2020-03-08\n"+
+			"ERROR: INDIVIDUAL: US01: @I34@: Birth date 2027-07-07 is after current date 2020-03-08\n"+
+			"ERROR: INDIVIDUAL: US01: @I35@: Birth date 2030-10-10 is after current date 2020-03-08\n"+
+			"ERROR: INDIVIDUAL: US01: @I36@: Birth date 2030-10-10 is after current date 2020-03-08\n"+
+			"ERROR: INDIVIDUAL: US01: @I37@: Birth date 2030-10-10 is after current date 2020-03-08\n"+
+			"ERROR: INDIVIDUAL: US01: @I38@: Birth date 2030-10-10 is after current date 2020-03-08\n"+
+			"ERROR: INDIVIDUAL: US01: @I39@: Birth date 2032-02-18 is after current date 2020-03-08\n"+
+			"ERROR: INDIVIDUAL: US01: @I40@: Birth date 2032-02-18 is after current date 2020-03-08\n"+
+			"ERROR: INDIVIDUAL: US01: @I41@: Birth date 2032-02-18 is after current date 2020-03-08\n"+
+			"ERROR: INDIVIDUAL: US01: @I42@: Birth date 2032-02-18 is after current date 2020-03-08\n"+
+			"ERROR: INDIVIDUAL: US01: @I43@: Birth date 2032-02-18 is after current date 2020-03-08\n"+
+			"ERROR: INDIVIDUAL: US01: @I44@: Birth date 2032-02-18 is after current date 2020-03-08\n"+
+			"ERROR: INDIVIDUAL: US01: @I44@: Marriage date 2020-05-10 is after current date 2020-03-08\n")
 
 if __name__ == "__main__":
     unittest.main()
